238dddcd8151fa3faa1e1f967f7a7bc34a49c4c6
Flaviano Dias <flavindias@msn.com>
2022-06-14 06:50:01 -0300

    🐛 update return code

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -3,6 +3,6 @@ import { Router, Response } from 'express';
 const router = Router();
 
 export const deejaiRoutes = () => {
-    router.get('/', (_, res: Response) => res.status(413).json({ message: 'Deej.ai API' }));  
+    router.get('/', (_, res: Response) => res.status(418).json({ message: 'Deej.ai API' }));  
     return router;
 };
