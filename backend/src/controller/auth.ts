2f1a5c1de98aff1e970d80ad0dd326cecd078b05
Flaviano Dias <flavindias@msn.com>
2022-06-14 07:00:49 -0300

    🚧 get info from spotify

---
+++ backend/src/controller/auth.ts
@@ -0,0 +1,24 @@
+import axios from 'axios';
+import dotenv from 'dotenv';
+import { Request, Response } from 'express';
+dotenv.config();
+
+export const AuthController = {
+    async spotify(req: Request, res: Response) {
+        try{
+            const { token } = req.body;
+            const response = await axios.get(`https://api.spotify.com/v1/me`, {
+                headers: {
+                    Authorization: `Bearer ${token}`
+                }
+            });
+            return res.status(200).json(response.data);
+        }
+        catch(err){
+            console.log(err);
+            return res.status(500).json({
+                message: 'Error while trying to authenticate with Spotify'
+            });
+        }
+    }
+}
--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,8 +1,9 @@
 import { Router, Response } from 'express';
-
+import { AuthController } from '../controller/auth';
 const router = Router();
 
 export const deejaiRoutes = () => {
     router.get('/', (_, res: Response) => res.status(418).json({ message: 'Deej.ai API' }));  
+    router.post('/auth/spotify', AuthController.spotify);
     return router;
 };
