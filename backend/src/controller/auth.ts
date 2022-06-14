a9c033b21f6f84ed29282cc71412a48e9ab47dd3
Flaviano Dias <flavindias@msn.com>
2022-06-14 07:05:56 -0300

    🚧 deezer get user info

--- backend/src/controller/auth.ts
+++ backend/src/controller/auth.ts
@@ -20,5 +20,22 @@ export const AuthController = {
                 message: 'Error while trying to authenticate with Spotify'
             });
         }
+    },
+    async deezer(req: Request, res: Response) {
+        try{
+            const { token } = req.body;
+            const response = await axios.get(`https://api.deezer.com/user/me`, {
+                headers: {
+                    Authorization: `Bearer ${token}`
+                }
+            });
+            return res.status(200).json(response.data);
+        }
+        catch(err){
+            console.log(err);
+            return res.status(500).json({
+                message: 'Error while trying to authenticate with Deezer'
+            });
+        }
     }
 }
