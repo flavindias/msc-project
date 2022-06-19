5c6277798bc1c4f985448025ec7bb7ad1141926e
Flaviano Dias <flavindias@msn.com>
2022-06-19 15:15:06 -0300

    ✨ another way to login with pass and email

--- backend/src/controller/auth.ts
+++ backend/src/controller/auth.ts
@@ -1,9 +1,14 @@
 import axios from "axios";
+import dotenv from "dotenv";
+import { sign } from "jsonwebtoken";
+import { compare, hashSync } from "bcryptjs";
 import { Request, Response } from "express";
 import { getToken, getTracks } from "./deezer";
 import { PrismaClient, User, SpotifyInfo, DeezerInfo } from "@prisma/client";
+dotenv.config();
 
 const prisma = new PrismaClient();
+const { JWT_SECRET } = process.env;
 
 const findUser = async (email: string) => {
   const user = await prisma.user.findFirst({
@@ -17,6 +22,8 @@ const findUser = async (email: string) => {
   });
   return user as User & { spotify: SpotifyInfo; deezer: DeezerInfo };
 };
+
+const token = (id: string) => sign({ id }, `${JWT_SECRET}`);
 
 export const AuthController = {
   async spotify(req: Request, res: Response) {
@@ -41,12 +48,14 @@ export const AuthController = {
           data: {
             email: response.data.email,
             name: response.data.display_name,
+            password: hashSync((Math.random() + 1).toString(36).substring(7), 10),
             spotify: {
               create: spotify,
             },
           },
         });
-        return res.status(200).json(newUser);
+        
+        return res.status(200).json({user: newUser, token: token(newUser.id) });
       }
       if (!user.spotify) {
         const updatedUser = await prisma.user.update({
@@ -59,10 +68,10 @@ export const AuthController = {
             },
           },
         });
-        return res.status(200).json(updatedUser);
+        return res.status(200).json({user: updatedUser, token: token(updatedUser.id) });
       }
 
-      return res.status(200).json(user);
+      return res.status(200).json({user, token: token(user.id) });
     } catch (err) {
       console.log(err);
       return res.status(500).json({
@@ -70,6 +79,7 @@ export const AuthController = {
       });
     }
   },
+
   async deezer(req: Request, res: Response) {
     try {
       const { token } = req.body;
@@ -91,13 +101,14 @@ export const AuthController = {
         const newUser = await prisma.user.create({
           data: {
             email: response.data.email,
+            password: hashSync((Math.random() + 1).toString(36).substring(7), 10),
             name: response.data.name,
             deezer: {
               create: deezer,
             },
           },
         });
-        return res.status(200).json(newUser);
+        return res.status(200).json({user:newUser, token: token(newUser.id) });
       }
       if (!user.deezer) {
         const updatedUser = await prisma.user.update({
@@ -110,11 +121,11 @@ export const AuthController = {
             },
           },
         });
-        return res.status(200).json(updatedUser);
+        return res.status(200).json({user:updatedUser, token: token(updatedUser.id) });
       }
       await getTracks(authToken);
       
-      return res.status(200).json(user);
+      return res.status(200).json({user, token: token(user.id) });
     } catch (err) {
       console.log(err);
       return res.status(500).json({
@@ -122,4 +133,21 @@ export const AuthController = {
       });
     }
   },
+  async login(req: Request, res: Response) {
+    try {
+      const { email, password } = req.body;
+      if(!email || !password) throw new Error("Email or password is missing");
+      const user = await findUser(email);
+      if (!user) throw new Error("User not found");
+      const isValid = await compare(password, `${user.password}`);
+      if (!isValid) throw new Error("Invalid email or password");
+      
+      return res.status(200).json({user, token: token(user.id) });
+    }
+    catch (err) {
+      return res.status(500).json({
+        message: "Error while trying to authenticate",
+      });
+    }
+  }
 };
--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -6,5 +6,6 @@ export const deejaiRoutes = () => {
     router.get('/', (_, res: Response) => res.status(418).json({ message: 'Deej.ai API' }));  
     router.post('/auth/spotify', AuthController.spotify);
     router.post('/auth/deezer', AuthController.deezer);
+    router.post('/auth/login', AuthController.login);
     return router;
 };
