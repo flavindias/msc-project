c54b847a01f93b866315738e55145884f5780dc9
Flaviano Dias <flavindias@msn.com>
2022-06-19 14:34:43 -0300

    🔥 removing kafka from middleware

--- backend/src/middlewares/deezer.ts
+++ backend/src/middlewares/deezer.ts
@@ -1,14 +1,6 @@
 
 import dotenv from "dotenv";
-import { Kafka } from "kafkajs";
 dotenv.config()
-
-const { KAFKA_BROKERS } = process.env;
-const kafka = new Kafka({
-    clientId: "deejai-deezer-consumer",
-    brokers: `${KAFKA_BROKERS}`.split(",")
-});
-const consumer = kafka.consumer({ groupId: 'deezer-group' });
 
 export const getTrackInfo = () => {
     console.log('getTrackInfo');
